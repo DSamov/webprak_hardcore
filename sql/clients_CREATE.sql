29895f14a703956a6e302341b179fe1a26d1b12b
DSamov <54376172+DSamov@users.noreply.github.com>
2025-05-22 22:21:12 +0300

    Delete sql directory

--- sql/clients_CREATE.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS clients CASCADE;
-CREATE TABLE clients (
-                         id SERIAL,
-                         surname TEXT,
-                         name TEXT NOT NULL,
-                         patron TEXT,
-                         PRIMARY KEY (id)
-);
--- sql/clients_contact_data_CREATE.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS ClientInstances CASCADE;
-CREATE TABLE ClientInstances (
-                                 id INTEGER NOT NULL,
-                                 phone character(11) NOT NULL,
-                                 email TEXT,
-                                 FOREIGN KEY (id) REFERENCES ClientsData ON DELETE CASCADE
-);
